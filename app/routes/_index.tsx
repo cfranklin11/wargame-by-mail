bc3d1b4b65830fa0bd812ba8c56340d43907b5b1
Craig Franklin <craigjfranklin@gmail.com>
2024-06-19 22:10:07 +1000

    Add Prettier & remove some options from .editorconfig
    Running Prettier made me realise that the Phaser template set
    the default tabsize to 4 spaces, among other things, so I tweaked
    the config and formatted the files.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,29 +3,27 @@ import { ClientOnly } from "remix-utils/client-only";
 import App from "../app.client";
 
 export const links: LinksFunction = () => {
-    return [
-        {
-            rel: "stylesheet",
-            href: "styles/globals.css",
-        },
-    ];
+  return [
+    {
+      rel: "stylesheet",
+      href: "styles/globals.css",
+    },
+  ];
 };
 
 export const meta: MetaFunction = () => {
-    return [
-        { title: "Phaser Remix Template" },
-        {
-            name: "description",
-            content:
-                "A Phaser 3 Remix project template that demonstrates Remix with React communication and uses Vite for bundling.",
-        },
-    ];
+  return [
+    { title: "Phaser Remix Template" },
+    {
+      name: "description",
+      content:
+        "A Phaser 3 Remix project template that demonstrates Remix with React communication and uses Vite for bundling.",
+    },
+  ];
 };
 
 export default function Index() {
-    return (
-        <ClientOnly fallback={<h3>Loading Game...</h3>}>
-            {() => <App />}
-        </ClientOnly>
-    );
+  return (
+    <ClientOnly fallback={<h3>Loading Game...</h3>}>{() => <App />}</ClientOnly>
+  );
 }
--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -6,26 +6,27 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 // This is the subfolder where the app will be deployed, if you want to change it, you can do it here like this: const subfolder = '/my-subfolder-name/';
-const subfolder = '/';
+const subfolder = "/";
 
 export default defineConfig({
-    base: subfolder,
-    plugins: [
-        remix({
-            ssr: false,
-            basename: subfolder,
-            
-            buildEnd: () => {
-                const line = "---------------------------------------------------------";
-                const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
-                process.stdout.write(`${line}\n${msg}\n${line}\n`);
-                process.stdout.write(`✨ Done ✨\n`);
-            }
-        }),
-        tsconfigPaths(),
-    ],
-    logLevel: 'warning',
-    server: {
-        port: 8080
-    }
+  base: subfolder,
+  plugins: [
+    remix({
+      ssr: false,
+      basename: subfolder,
+
+      buildEnd: () => {
+        const line =
+          "---------------------------------------------------------";
+        const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
+        process.stdout.write(`${line}\n${msg}\n${line}\n`);
+        process.stdout.write(`✨ Done ✨\n`);
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  logLevel: "warning",
+  server: {
+    port: 8080,
+  },
 });
