159a09eab83f49be0b402698a37d1942d3d0a0bd
Craig Franklin <craigjfranklin@gmail.com>
2024-08-04 21:33:23 +1000

    Add user module with validation function

--- app/.server/db.ts
+++ app/.server/db.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { z } from "zod";
 import bcrypt from "bcryptjs";
 
 import { wrap } from "~/utils/array";
 import { validate as validateArmy } from "~/models/army";
 import { validate as validateUnit } from "~/models/unit";
 import { validate as validateMiniature } from "~/models/miniature";
+import { validate as validateUser } from "~/models/user";
 
 export type {
   Game,
@@ -17,41 +17,14 @@ export type {
   Miniature,
 } from "@prisma/client";
 
-const SHORT_TEXT_LIMIT = 255;
-const MIN_PASSWORD_LENGTH = 8;
-const MIN_REQUIRED_TEXT = 1;
-
 // Basic client for use in validations
 const prisma = new PrismaClient();
-
-const shortTextValidations = z
-  .string()
-  .min(MIN_REQUIRED_TEXT)
-  .max(SHORT_TEXT_LIMIT);
-
-const UserCreateInput = z.object({
-  email: shortTextValidations.email().refine(
-    async (email) => {
-      const user = await prisma.user.findUnique({ where: { email } });
-      return !user;
-    },
-    { message: "An account for this email already exists" },
-  ),
-  password: z.string().min(MIN_PASSWORD_LENGTH).max(SHORT_TEXT_LIMIT),
-  username: shortTextValidations.refine(
-    async (username) => {
-      const user = await prisma.user.findUnique({ where: { username } });
-      return !user;
-    },
-    { message: "Username already taken" },
-  ),
-});
 
 const db = new PrismaClient().$extends({
   query: {
     user: {
       create: async ({ args, query }) => {
-        await UserCreateInput.parseAsync(args.data);
+        await validateUser(prisma, args.data);
         const password = args.data.password;
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
@@ -63,7 +36,7 @@ const db = new PrismaClient().$extends({
       createMany: async ({ args, query }) => {
         const data = await Promise.all(
           wrap(args.data).map(async (userData) => {
-            await UserCreateInput.parseAsync(userData);
+            await validateUser(prisma, userData);
             const password = userData.password;
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash(password, salt);
---
+++ test/factories/user.ts
@@ -0,0 +1,24 @@
+import { Factory } from "fishery";
+import { faker } from "@faker-js/faker";
+
+import { User } from "../../app/.server/db";
+
+export const userInputFactory = Factory.define<
+  Omit<User, "id" | "updatedAt" | "createdAt">
+>(() => {
+  return {
+    username: faker.company.name(),
+    email: faker.internet.email(),
+    password: faker.internet.password({ length: 10 }),
+  };
+});
+
+export const userFactory = Factory.define<User>(({ sequence }) => {
+  const createdAt = faker.date.past();
+  return {
+    id: sequence,
+    createdAt,
+    updatedAt: faker.date.between({ from: createdAt, to: new Date() }),
+    ...userInputFactory.build(),
+  };
+});
