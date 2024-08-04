700e3e92df87a9a612ea47d5da557f7af0f19205
Craig Franklin <craigjfranklin@gmail.com>
2024-08-04 14:28:04 +1000

    Add model factories

---
+++ test/factories/army.ts
@@ -0,0 +1,26 @@
+import { Factory } from "fishery";
+
+import { Army } from "../../app/.server/db";
+import { faker } from "@faker-js/faker";
+
+export const armyInputFactory = Factory.define<Partial<Army>>(() => {
+  return {
+    name: faker.company.name(),
+    gameSystem: faker.commerce.department(),
+    faction: faker.commerce.product(),
+    description: faker.lorem.paragraph(),
+  };
+});
+
+export const armyFactory = Factory.define<Army>(({ sequence }) => {
+  const createdAt = faker.date.past();
+  return {
+    id: sequence,
+    createdAt,
+    updatedAt: faker.date.between({ from: createdAt, to: new Date() }),
+    name: faker.company.name(),
+    gameSystem: faker.commerce.department(),
+    faction: faker.commerce.product(),
+    description: faker.lorem.paragraph(),
+  };
+});
