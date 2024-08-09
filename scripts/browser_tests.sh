c2f91e74391532f9f7e13c85fb52a225f67f7fc7
Craig Franklin <craigjfranklin@gmail.com>
2024-08-09 15:02:42 +1000

    Drop test DB as part of setup instead of teardown

--- scripts/browser_tests.sh
+++ scripts/browser_tests.sh
@@ -20,11 +20,11 @@ function log_errors() {
 
 function clean_up() {
   export DATABASE_URL=$ORIGINAL_DATABASE_URL
-  docker-compose exec db psql -c "DROP DATABASE IF EXISTS test;" -U $DATABASE_USER
 
   exit ${EXIT_CODE}
 }
 
+docker-compose exec db psql -c "DROP DATABASE IF EXISTS test;" -U $DATABASE_USER
 docker-compose exec db psql -c "CREATE DATABASE test;" -U $DATABASE_USER
 
 PORT=7357 npm run test:e2e:local
